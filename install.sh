c346ae2032e33e087e278606f787e424efee28b3
Neg <92654015+NegK01@users.noreply.github.com>
2022-01-10 10:01:48 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -30,8 +30,8 @@ npm i fluent-ffmpeg -y
 npm i @adiwajshing/baileys -y
 npm i performance-now -y
 npm i form-data -y
-npm i yt-search -y
-npm i google-it 
+npm i yt-search -y 
+npm i g-i-s -y
 npm i hxz-api -y
 npm i cheerio -y
 npm i qrcode -y
